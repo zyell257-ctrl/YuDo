7d19c5c1d8cfd5d1c0122a77af3082c6c5e1ec05
xillzy <zyell257@gmail.com>
2026-05-14 16:33:47 +0700

    Fix public storage links on deploy

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'url' => env('FILESYSTEM_PUBLIC_URL', '/storage'),
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
